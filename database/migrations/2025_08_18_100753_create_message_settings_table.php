e06463ceecfebbed5368e196075256909f5faa22
getleadconnect <getleadconnect@gmail.com>
2025-10-31 18:15:12 +0530

    whatsapp notifications

--- database/migrations/2025_08_18_100753_create_message_settings_table.php
+++ database/migrations/2025_08_18_100753_create_message_settings_table.php
@@ -13,18 +13,14 @@ return new class extends Migration
     {
         Schema::create('message_settings', function (Blueprint $table) {
             $table->id();
-            $table->string('message_type', 50);
+            $table->string('vendor_name', 50);
             $table->string('whatsapp_api')->nullable();
-            $table->string('token')->nullable();
-            $table->string('secret_key')->nullable();
-            $table->string('template_name')->nullable();
-            $table->text('template_text')->nullable();
+            $table->string('api_token')->nullable();
             $table->boolean('status')->default(true);
             $table->unsignedBigInteger('created_by')->nullable();
             $table->timestamps();
             
             $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
-            $table->index('message_type');
             $table->index('status');
         });
     }
